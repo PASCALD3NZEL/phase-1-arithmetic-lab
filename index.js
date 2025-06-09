a54bb523cd58ee6c49d669adf770c584cd066715
Pascal <pascaldenzel7@gmail.com>
2025-06-09 22:35:21 +0300

    Complete math functions lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+let num3 = 19;
+let num4 = 15;
+let mod = num3 % num4; // Calculates the remainder
+let max = Math.max(20, 10, 15, 5); // Returns the highest number in the set     
+// Exporting the variables to be used in tests
